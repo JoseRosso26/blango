738548dc41b6db6753adee321002945aeffc6223
José Carlos Rosso Rosales <jose_rr_jk@hotmail.com>
2022-10-03 03:18:49 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,16 @@
+from django import template
+register = template.Library()
+
+from django.contrib.auth import get_user_model
+user_model = get_user_model()
+
+@register.filter
+def author_details(author):
+    if not isinstance(author, user_model):
+        # retorna una cadena vacia como valor para evitar fallos
+        return ''
+    if author.first_name and author.last_name:
+        name = f'{author.first_name} {author.last_name}'
+    else:
+        name = f'{author.username}'
+    return name
