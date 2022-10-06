cbd343b263b5876cf5a3c89b45821f985d3ab1ea
José Carlos Rosso Rosales <jose_rr_jk@hotmail.com>
2022-10-06 15:55:45 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,28 +1,52 @@
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
 register = template.Library()
 
 from django.contrib.auth import get_user_model
 user_model = get_user_model()
 
 @register.filter
-def author_details(author, current_user=None):
+def author_details(author, current_user):
     if not isinstance(author, user_model):
-        # retorna una cadena vacia como valor para evitar fallos
-        return ''
+        # return empty string as safe default
+        return ""
 
     if author == current_user:
-        return format_html('<strong>me</strong>')
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
-        name = f'{author.first_name} {author.last_name}'
+        name = f"{author.first_name} {author.last_name}"
     else:
-        name = f'{author.username}'
-        
+        name = f"{author.username}"
+
     if author.email:
         prefix = format_html('<a href="mailto:{}">', author.email)
-        suffix = format_html('</a>')
+        suffix = format_html("</a>")
     else:
-        prefix = ''
-        suffix = ''
+        prefix = ""
+        suffix = ""
+
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag
+def row(extra_classes=''):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.inclusion_tag('blog/post-list.html',takes_context=True)
+def recent_posts(context):
+    post = context['post']
+    posts = Post.objects.order_by('-published_at').exclude(pk=post.pk)[:5]
+    return {'title': 'Recent Post','posts': posts}
